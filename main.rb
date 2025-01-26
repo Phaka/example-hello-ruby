53ec89badb522ac50549acf473771e3774c271a8
Werner Strydom <bloudraak@users.noreply.github.com>
2025-01-26 07:08:42 -0800

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+# Hello World in Ruby
+# Ruby is designed for programmer happiness with elegant, natural syntax
+# Everything in Ruby is an object - even numbers and simple values
+# This script can be run directly if the file is marked as executable
+
+puts "Hello, World!"
